71ed25498b0334d0bfdbb77b7de979f8bdd9791b
Sergey Matveev <onlyforflash@mail.ru>
2024-06-10 21:48:04 +0300

    added some info for person page

--- src/components/PersonInfo.vue
+++ src/components/PersonInfo.vue
@@ -12,7 +12,48 @@
       <v-app-bar-title>
         ЗЖ-У
       </v-app-bar-title>
-    </v-app-bar> 
+    </v-app-bar>
+    <div class="mt-16 d-flex align-center h-100 justify-center">
+      <v-form class="w-25">
+        <div>
+          <p> Выберите ваш пол </p>
+          <v-radio-group>
+            <v-radio 
+              label="Мужской" 
+              value="male"
+            />
+            <v-radio 
+              label="Женский" 
+              value="female"
+            />
+          </v-radio-group>
+        </div>
+        <div>
+          <p> Введите ваш возраст </p>
+          <v-text-field>
+
+          </v-text-field>
+        </div>
+        <div>
+          <p> Введите ваш рост </p>
+          <v-text-field>
+
+          </v-text-field>
+        </div>
+        <div>
+          <p> Введите ваш вес </p>
+          <v-text-field>
+
+          </v-text-field>          
+        </div>
+        <div>
+          <v-select
+            label="Уровень вашей активности"
+            :items="['Минимальная активность', 'Слабый уровень активности', 'умеренный уровень активности', 'Тяжелая аткивность', 'Экстремальный уровень']"
+          />
+        </div>
+      </v-form> 
+    </div>
   </v-app>
 </template>
 
